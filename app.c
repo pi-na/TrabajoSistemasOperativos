4b726d426852c5e686493b7eb82e4f8c374255a7
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-09-12 19:38:18 -0300

    SLAVE Se termino el slave. Se implementaron validaciones para todos los llamados a funciones del SO.
    Co-authored-by: jrambau <jrambau@users.noreply.github.com>
    Co-authored-by: za0sec <jperalb@gmail.com>

--- app.c
+++ app.c
@@ -4,6 +4,21 @@
 #include <sys/stat.h>           // file-stat nos da info. sobre el path
 #include "list.h"
 
+/*
+TODO BORRAR ESTO
+pipefd   0     1
+padre    0=====]    
+         [=====0       hijo
+
+argv[0] -> "slave.c"
+argv[1] -> "/path/file.f"
+
+Si se cierra un pipe, el que lee recibe EOF
+*/
+
+
+
+
 int main(int argc, char *argv[]){
 
     if (argc < 2) {
@@ -11,22 +26,14 @@ int main(int argc, char *argv[]){
         return EXIT_FAILURE;
     }
 
-    /*
-    Iterate through the array
-    For each element of the array, call my files function
-    */
-    for(int i = 1; i<argc; i++){
-        FILE *file = fopen(argv[i], "r");
-        
-    }
+    
 
 }
-
 /*
 Recibe un array de path de files validos
 Retorna una lista con los file-path cargados a una lista ordenada por tamano 
 */
-listADT getFiless(char * exec_path, int total_files, char* files[]) {
+listADT get_files(char * exec_path, int total_files, char* files[]) {
     struct stat file_stat;
     char full_path[1024];
     char * file_path;                              // este directory stream queda abierto y se itera llamando a readdir() o rewinddir()
@@ -50,50 +57,4 @@ listADT getFiless(char * exec_path, int total_files, char* files[]) {
     }
 
     return list;
-}
-
-
-
-
-
-
-void listDirectory(const char * path){
-    
-    DIR *dir = opendir(path);
-    struct dirent *entry;
-    struct stat file_stat;
-    char full_path[1024];
-
-    if(dir == NULL){  
-        perror("Error opening directory");  //si no abre error
-        return;
-    }
-
-
-    while((entry = readdir(dir)) != NULL){
-         if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) { //omito los "." y ".."
-            continue;
-        }
-
-        snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
-
-        if (stat(full_path, &file_stat) == -1) {
-            perror("stat");
-        }
-
-        if(S_ISDIR(file_stat.st_mode)){ //chequeo si es un directorio o un archivo
-            printf("d");
-            tab(level + 1);
-            printf("%s\n", entry->d_name);
-            listDirectory(full_path, level + 1); //si es un directorio hago recursividad 
-        }else{
-            printf("f");
-            tab(level + 1);
-            printf("%s\n",entry->d_name); 
-        }
-    }
-
-    closedir(dir);
-
-    return;
 }
--- slave.c
+++ slave.c
@@ -3,64 +3,127 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
-#define MD5_EXEC "/usr/bin/md5sum"
-#define MD5_LENGTH 32
+#define MD5_BIN_PATH "/usr/bin/md5sum"
+#define MD5_HASH_SIZE 32
+#define BUFF_SIZE 1024
+#define MD5_OUTPUT_SEPARATORS 2
 
-//valido que me hayan enviado un path
-//fork -> exec MD5:
-//      - abro pipe
-//      - cierro las entradas q quedan duplicados
-//      - le paso como args el path
+void process_md5_hash(char *file_path);
+size_t read_md5(char *path, int pipe_fd[], char *buff);
+void exec_md5(char *path, int pipe_fd[]);
+
+
 int main(int argc, char *argv[]){
+    if(argc > 1){
+        for(int i = 1; i < argc; i++){
+            process_md5_hash(argv[i]);
+        }
+        return EXIT_SUCCESS;
+    } 
 
-    // TODO: CHEQUEAR QUE ME PASARON UN FILE (VER EJ GUIA)
-    if (argc < 2) {
-        printf("Debes pasar un archivo como argumento.\n");
+    int file_path_size = 0;
+    int EOF_flag = 0;
+    char *file_path = malloc(BUFF_SIZE);
+
+    if(file_path == NULL){
+        perror("Error al reservar memoria para file_path");
         return EXIT_FAILURE;
     }
 
-    int pipefd[2];                      //pipe() deja en pipefd[0] y pipefd[1] los FD del pipe
-    if(pipe(pipefd) == -1){
-        perror("Error al pipear");
-        return EXIT_FAILURE;
+    while(!EOF_flag){
+        EOF_flag = read(STDIN_FILENO, file_path + file_path_size, 1) == 0;
+
+        if(EOF_flag){            
+            if(file_path_size == 0)                 // Si encontre un EOF, podria tener un ultimo file_path para procesar
+                continue;
+        } else if(file_path[file_path_size] != '\n'){
+            file_path_size++;
+            continue;
+        }
+
+        // Acabo de leer una string terminada en \n o una string + EOF!
+        file_path_size++;
+        file_path[file_path_size - 1] = '\0'; 
+        process_md5_hash(file_path);
+        file_path_size = 0;
     }
 
-    switch(fork()){
-        case(-1):
-            printf("FALLO EL FORK!!\n");
-            return EXIT_FAILURE;
-
-        case(0):
-            close(pipefd[0]);   // CIERRO FD 0 DEL PIPE (LO USARA EL PADRE)
-            close(1);           // CIERRO FD 1 PARA QUE NO TENGA STDOUT
-            dup(pipefd[1]);     // DUPLICA EL FD DEL EXTREMO ABIERTO DEL PIPE Y LO ASIGNA AL FD MAS CHICO: EL 1
-            close(pipefd[1]);   // Cerramos el FD original de escritura del pipe, ya está duplicado
-            char *const argvMD5[] = {MD5_EXEC, argv[1], NULL};
-            execve(MD5_EXEC, argvMD5, NULL);
-            break;
-
-        default:
-            close(pipefd[1]);   // CIERRO FD 1 DEL PIPE (LO USARA EL HIJO)
-            char md5_hash[MD5_LENGTH];
-            read(pipefd[0], md5_hash, MD5_LENGTH);
-            printf("El MD5 de %s es %s\n", argv[1], md5_hash);
-            close(pipefd[0]);   // Cerramos el extremo de lectura después de leer
-            break;
-    }
-
+    free(file_path);
     return EXIT_SUCCESS;
 }
 
+
 /*
-pipefd   0     1
-padre    0=====]    
-         [=====0       hijo
-
-argv[0] -> "slave.c"
-argv[1] -> "/path/file.f"
+Recibe un file y escribe por salida estandar su hash MD5.
 */
+void process_md5_hash(char *file_path){
+    int pipe_fd[2]; 
+    char md5_result[BUFF_SIZE];
+    size_t md5_result_size;
+
+    if(pipe(pipe_fd) == -1){
+        perror("Error al pipear");
+        free(file_path);
+        exit(EXIT_FAILURE);
+    }
+    
+    int forkpid;
+    
+    switch(forkpid = fork()){
+        case(-1):
+            perror("ERROR en fork(). Terminando la ejecucion...\n");
+            free(file_path);
+            exit(EXIT_FAILURE);
+        case(0):
+            exec_md5(file_path, pipe_fd);
+        default:
+            md5_result_size = read_md5(file_path, pipe_fd, md5_result);
+    }
+
+    write(STDOUT_FILENO, md5_result, md5_result_size);
+}
 
 
+/*
+Espera que el proceso hijo MD5 termine y lee el hash resultante desde el pipe.
+Deja el resultado en el buffer que recibe por parametro.
+*/
+size_t read_md5(char *path, int pipe_fd[], char *buff){
+    int status;
+    close(pipe_fd[1]);
+    wait(&status);
+
+    if(!WIFEXITED(status)){
+        fprintf(stderr, "Hubo un error en el procesamiento del hash MD5 para el archivo %s\n", path);
+        free(path);
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t bytes_read = read(pipe_fd[0], buff, BUFF_SIZE);
+
+    if (bytes_read == -1) {
+        perror("Error al leer del pipe");
+        free(path);
+        exit(EXIT_FAILURE);
+    }
+
+    close(pipe_fd[0]);
+    return (size_t)bytes_read;            
+}
 
 
+/*
+Recibe un path y devuelve el md5 del archivo. 
+Prepara los args y pipes para ejecutar el md5sum.
+*/
+void exec_md5(char *path, int pipe_fd[]){
+    close(pipe_fd[0]);   
+    close(1);           
+    dup(pipe_fd[1]);     
+    close(pipe_fd[1]);   
+    char *const argv_md5[] = {MD5_BIN_PATH, path, NULL};
+    execve(MD5_BIN_PATH, argv_md5, NULL);
+}
