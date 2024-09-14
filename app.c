51dc71e18a71b10b8c36461cc3dd19e67310412a
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-09-14 10:45:23 -0300

    Thoughts & prayers

--- app.c
+++ app.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>           // file-stat nos da info. sobre el path
 #include "list.h"
 
+#define SLAVE_BIN_PATH "./slave"
 /*
 TODO BORRAR ESTO
 pipefd   0     1
@@ -22,16 +23,71 @@ Si se cierra un pipe, el que lee recibe EOF
 int main(int argc, char *argv[]){
 
     if (argc < 2) {
-        printf("You must pass a file or a list of files as an argument.\n");
+        printf("Debes enviar uno o mas archivos por argumento para comenzar la ejecucion.\n");
         return EXIT_FAILURE;
     }
 
-    
+        int total_slaves = argc / 2;
+    init_slaves(total_slaves);
+
+    return EXIT_SUCCESS;
 
 }
+
+/*
+    Retorna un array de pipes, donde cada pipe[i] es un array de 2 enteros, siendo el primero el write end y el segundo el read end
+    El indice del array es representativo de cada slave
+*/
+int** init_slaves(int total_slaves){
+    int pipes[total_slaves][2];
+    for(int i = 1; i < total_slaves; i++){
+        
+    }
+}
+
+
+    // POR AHORA HAGO UN SLAVE CADA 2 FILES!
+    /*
+    quiero implementar el proceso de levantar los slaves y de hacer select() para esperar a que esten listos
+    > levanto (argc - 1) / 2 slaves
+        > Esto implica levantar sus respectivos pipes ( CERRANDO LOS NECESARIOS! )
+        > Supongo que hago un for y voy haciendo muchos fork, necesito cargar los PID en un array?
+          quizas necesito mapear pid de los hijos a los pipes indicados. debo investigar como funciona select()!
+        > Por ahora apenas me contesta un slave imprimo su output
+          si funcionan bien los slaves, no deberia tener problema "interfaceando" con ellos
+        > en el slave debo cerrar todos los pipes menos el indicado
+          uso el array de todos los pipes, lo recorro y cierro todos
+          ANTES de hacer el execve()
+    */
+/*
+    Inicializa un slave, retornando un array de 2 enteros, 
+    siendo el primero el write end y el segundo el read end del pipe asociado
+*/
+// TODO Pasar por parametro un array donde dejar los pipes???
+int* init_slave(){
+    int pipe_fd[2];
+
+    if(pipe(pipe_fd) == -1){
+        perror("Error al pipear");
+        exit(EXIT_FAILURE);
+    }
+
+    switch(fork()){
+        case -1:
+            perror("fork");
+            exit(EXIT_FAILURE);
+        case 0:
+            char *const argv_slave[] = {SLAVE_BIN_PATH, NULL};
+            execve(SLAVE_BIN_PATH, argv_slave, NULL);
+            break;
+        default:
+            return getpid();
+    }
+}
+
 /*
 Recibe un array de path de files validos
-Retorna una lista con los file-path cargados a una lista ordenada por tamano 
+Retorna una lista con una cola de files, ordenada por tamano 
 */
 listADT get_files(char * exec_path, int total_files, char* files[]) {
     struct stat file_stat;
@@ -48,7 +104,7 @@ listADT get_files(char * exec_path, int total_files, char* files[]) {
             perror("stat");
         }
 
-        // Si es un DIR, lo salteo  (la consigna dice que es innecesario)
+        // Si es un DIR, lo salteo 
         if(S_ISDIR(file_stat.st_mode)){
             continue;
         }
